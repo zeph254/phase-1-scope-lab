4a90834881afd1bc92749f51ef28f2c813e2443c
zephania owuor <ularezephaniah@gmail.com>
2024-10-01 19:03:47 +0300

    scope-lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution in this file!
+var customerName = bob
+function upperCaseCustomer() {
+    customerName = customerName.toUpperCase();
+}
+var bestCustomer; function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'some initial value';
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'some new value';
+}
